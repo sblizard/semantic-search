b1a97f6612dc553a89480fc13c36c869d56e462f
Sean Blizard <emailseanblizard@gmail.com>
2024-10-13 11:36:42 -0400

    Hyphothetical document route established

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from contextlib import asynccontextmanager
 #local imports
 from src.io import Query, ChatGPTResponse, GetEmbeddingParams, EmbeddingOutput, UpsertInput, SearchOutput, SearchQuery
 from src.query.hyphoteticalDocument import create_hyphotetical_document
-from src.embed import upsert_vectors, get_embedding, semantic_search
+from src.embed import upsert_vectors, get_embedding, semantic_search, search_by_embedding
 
 
 @asynccontextmanager
@@ -26,11 +26,18 @@ def read_root():
 
 @app.post("/query")
 async def search(query: Query):
-    #TODO: create hyphotetical document
-    #TODO: embed hyphotetical document
-    #TODO: search
-    #TODO: return results
-    return {"query": query.query}
+    print("**********************")
+    print(f'query: {query}')
+    chatGPTResponse: ChatGPTResponse = await create_hyphotetical_document(query=query)
+    print(f'GPT Response: {chatGPTResponse}')
+    embeddedParams: GetEmbeddingParams = GetEmbeddingParams(text=chatGPTResponse.response)
+    print(f' embeddedParams: {embeddedParams}')
+    gptEmbedding: EmbeddingOutput = await get_embedding(params=embeddedParams)
+    print(f'gptEmbedding: {gptEmbedding}')
+    searchResults: SearchOutput = await search_by_embedding(embedding=gptEmbedding)
+    print(f'searchResult: {searchResults}')
+    print("**********************")
+    return searchResults
 
 
 @app.get("/hdTest")
@@ -42,9 +49,10 @@ async def create_hyphotetical_document_test() -> ChatGPTResponse:
 
 @app.post("/embed")
 async def embed(text: GetEmbeddingParams) -> EmbeddingOutput:
-    embeddingOut: EmbeddingOutput = await get_embedding(text)
+    embeddingOut: EmbeddingOutput = await get_embedding(params=text)
     upsert_vectors(UpsertInput(data=embeddingOut, metadata={"text": text.text}))
     return embeddingOut
+
 
 @app.post("/search")
 async def search_endpoint(query: SearchQuery) -> SearchOutput:
